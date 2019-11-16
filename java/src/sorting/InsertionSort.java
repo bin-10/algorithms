f88e55e69b72e11ea0cdbcc4d9e69a38f9ff4471
Manoj Surimani <manoj.ms7117@gmail.com>
2019-11-16 12:48:24 +0530

    Fromat the code

--- java/src/sorting/InsertionSort.java
+++ java/src/sorting/InsertionSort.java
@@ -2,24 +2,24 @@ package sorting;
 
 public class InsertionSort {
 
-    private static void sort(int[] numbers){
-        int n=numbers.length;
-        for(int i=1;i<n;i++){
-            int currentNumber=numbers[i];
-            int j=i-1;
-            while(j>=0 && numbers[j]>currentNumber){
-                numbers[j+1]=numbers[j];
+    private static void sort(int[] numbers) {
+        int n = numbers.length;
+        for (int i = 1; i < n; i++) {
+            int currentNumber = numbers[i];
+            int j = i - 1;
+            while (j >= 0 && numbers[j] > currentNumber) {
+                numbers[j + 1] = numbers[j];
                 j--;
             }
-            numbers[j+1]=currentNumber;
+            numbers[j + 1] = currentNumber;
         }
     }
 
     public static void main(String[] args) {
-        int[] numbers={9,7,8,4,6,5,0,2,1,3};
+        int[] numbers = { 9, 7, 8, 4, 6, 5, 0, 2, 1, 3 };
         sort(numbers);
-        for (int number:numbers) {
-            System.out.print(number+" ");
+        for (int number : numbers) {
+            System.out.print(number + " ");
         }
     }
 }
